c2348cfaba787266bdc8caa57b7616e2f1b3a37c
Craig Christopher Messi <craig.messi@gmail.com>
2020-05-28 01:07:57 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,3 +14,6 @@
 ActiveRecord::Schema.define(version: 0) do
 
 end
+  t.string   "name"
+    t.string   "password_digest"
+rails g resources User name:string password_digest:string --no-test-framework
